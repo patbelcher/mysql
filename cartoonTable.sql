5af017847ef901fb2af3bc62b1ecf88cfc9d8923
Pat <patbelcher@yahoo.com>
2023-06-23 10:38:49 -0400

    sql databases

--- cartoonTable.sql
+++ cartoonTable.sql
@@ -1,12 +1,11 @@
 CREATE TABLE cartoon (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    cartoon_id VARCHAR(6)  NOT NULL,
+    cartoon_id int(6)  NOT NULL,
     title VARCHAR(50),
-    yearsProduced INT(30),
+    yearsProduced VARCHAR(30),
     genre VARCHAR(50),
-    director VARCHAR(50),
+    intended_audience VARCHAR(50),
     main_chars VARCHAR(250),
     cover_img VARCHAR(300),
-    producer VARCHAR(25),
     created_at DATETIME
     );
--- cartoonValues.sql
+++ cartoonValues.sql
@@ -1,16 +1,60 @@
-INSERT INTO movies VALUES (
+INSERT INTO cartoon VALUES (
     null,
     '001',
-    'Buggs Bunny',
-    '1940s - 1960s',
-    'Romantic Comedy',
-    '109',
-    'Unkniwn',
-    'Mrs. Robinson',
-    'A lot',
-    'https://upload.wikimedia.org/wikipedia/en/e/ea/Animalhouseposter.jpg',    
-    'R',
-    'Unknown',
-    '3M',
+    'Popeye',
+    '1929 - 1950s',
+    'Kids cartoons',
+    'kids and adults',
+    'Popeye, Olive, Wimpy, Bluto',
+    'https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/01/15/09/popeye.jpg',
     NOW()
 );
+
+INSERT INTO cartoon VALUES (
+    null,
+    '002',
+    'Buggs Bunny',
+    '1940s - 1960s',
+    'kids cartoons',
+    'adults, mature',
+    'Buggs, Porky, Daffy, Foghorn Leghorn',
+    ' https://static.tvtropes.org/pmwiki/pub/images/b46bae34e949a56ad2fc59c7ff422df6.jpg',    
+    NOW()
+);
+
+INSERT INTO cartoon VALUES (
+    null,
+    '003',
+    'The Simpsons',
+    '1989 - present',
+    'Stupid humor',
+    'kids - adults',
+    'Homer, Marge, Bart, Lisa',
+    'https://1000logos.net/wp-content/uploads/2021/05/The-Simpsons-logo.png',    
+    NOW()
+);
+
+INSERT INTO cartoon VALUES (
+    null,
+    '004',
+    'South Park',
+    '1997 - present',
+    'Raunchy humor',
+    'More adults than kids',
+    'Kyle, Stan, Cartman, Kenny',
+    'https://www.tvinsider.com/wp-content/uploads/2022/08/south-park-kyle-stan-1014x570.jpg',    
+    NOW()
+);
+
+INSERT INTO cartoon VALUES (
+    null,
+    '005',
+    'Family Guy',
+    '1999 - present',
+    'Raunchy humor',
+    'More adults than kids',
+    'Peter, Lois, Meg, Chris, Joe, Quagmire, Cleveland',
+    ' https://upload.wikimedia.org/wikipedia/en/thumb/a/aa/Family_Guy_Logo.svg/1200px-Family_Guy_Logo.svg.png',    
+    NOW()
+);
+
